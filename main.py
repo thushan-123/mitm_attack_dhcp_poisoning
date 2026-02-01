85e7de7f08a217d68326cb1eea9f812c6f7b649c
thushan-123 <thushanmadusanka456@gmail.com>
2026-02-01 19:33:38 +0530

    dhcp offer

--- main.py
+++ main.py
@@ -2,9 +2,11 @@
 from scapy.all import *
 import socket
 
+from scapy.layers.inet import IP
 from scapy.layers.l2 import Ether
 
 
-def main(source_mac):
+def dhcp_offer(source_mac):
     ethernet = Ether(dst="ff:ff:ff:ff:ff:ff", src=source_mac)
+    ip = IP(src="0.0.0.0", dst="255.255.255.255")
 
